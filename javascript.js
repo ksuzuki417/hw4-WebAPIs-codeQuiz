ebe1be99aa6bf6c26d11a8b12bb9395b1a366c64
Keita Suzuki <ksuzuki417@gmail.com>
2020-08-14 22:54:49 -0500

    more pseudo code added

--- javascript.js
+++ javascript.js
@@ -1,23 +1,43 @@
+//HTML DOM variables
 var startButton = document.getElementById("start-btn");
-var questionContainerEl = document.getElementById("question-container")
+var questionContainerEl = document.getElementById("question-container");
+var questionEl = document.getElementById("question-container")
+var answerButtonsEl = document.getElementById("answer-button");
 
-startButton.addEventListener("click", startGame)
+// keep track of current question index
+var currentQIndex = 0;
+var timerSecs = 90;
 
+//Start game
+startButton.addEventListener("click", startGame);
+
+//Start the timer
+
+//start the quizz
 function startGame() {
-    console.log("started")
-    startButton.classList.add("hide")
-    questionContainerEl.classList.remove("hide")
-    setNextQuestion()
+    console.log("started");
+    startButton.classList.add("hide");
+    currentQ = questions;
+    currentQIndex = 0;
+    questionContainerEl.classList.remove("hide");
+    setNextQuestion();
 }
 
+//setting next question
 function setNextQuestion() {
+    showQuestion(currentQ[currentQIndex]);
 
 }
 
+function showQuestion(quesiton) {
+    questionEl.innerText = question.question
+}
+//answer function
 function selectAnswer () {
     
 }
 
+//quesitons variable
 var questions = [
 {
     question: "What is 2 + 2",
@@ -28,3 +48,8 @@ var questions = [
 }
 
 ]
+
+// handle user answer click
+    // if answer was correct, then display "correct message"
+    // if answer was correct, then go to next question
+    // if answer was incorrect, then subtract 10 seconds from timer
