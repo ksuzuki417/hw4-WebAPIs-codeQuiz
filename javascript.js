925c76e9afb25ce4e4a0a5da77b24ed2db5ecbd5
Keita Suzuki <ksuzuki417@gmail.com>
2020-08-15 07:57:00 -0500

    style added can't display answers still

--- javascript.js
+++ javascript.js
@@ -3,11 +3,46 @@ var startButton = document.getElementById("start-btn");
 var questionContainerEl = document.getElementById("question-container");
 var questionEl = document.getElementById("question")
 var choicesEl = document.getElementById("answerChoices");
+var answerButton = document.getElementById("")
 var timerEl = document.getElementById("timer");
 
 // keep track of current question index
 var increment = 0;
 var timerSecs = 90;
+
+// count correct answers
+var correct = 0;
+
+//quesitons/answers variables
+var questions = [
+    {
+        question: "What does js stand for?",
+        answers: ["just stay", "just smart", "javascript", "joy script"],
+        correctAnswer: "javascript"
+    },
+    {
+        question: "Which HTML element is correct for JavaScript?",
+        answers: ["<script>", "<js>", "<javascript>", "<java>"],
+        correctAnswer: "0"
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        answers: ["<head>", "<body>", "between <head> and <body>", "all 3 places"],
+        correctAnswer: "<body>"
+    },
+    {
+        question: "How do you create function in JavaScript?",
+        answers: ["function myFunction ()", "function = myFunction", "function(myFunction())", "myFunction() = function"],
+        correctAnswer: "function myFunction"
+    },
+    {
+        question: "How do you add a comment in a JavaScript?",
+        answers: ["<!--This is a comment", "//This is a comment", ".This is a comment", "<This is a comment>"],
+        correctAnswer: "//This is a comment"
+    },
+    
+    ]
+    
 
 //Start game
 startButton.addEventListener("click", startQuiz);
@@ -19,8 +54,8 @@ startButton.addEventListener("click", startQuiz);
         console.log(timerSecs)
         timerSecs--}, 900);
         timerEl.append(timer);
+       
 //}
-
 
 
     // either timer <= 0 or all questions are answered to end the quiz
@@ -40,21 +75,21 @@ function nextQuestion() {
     } else {
         questionEl.textContent = questions[increment].question
         for (var i = 0; i < questions[increment].answers.length; i++) {
-            var answerChoicesEl = document.getElementById(answerChoices)
+            //var answerChoicesEl = document.getElementById(answerChoices)
+            //CANNOT show answer choices
+            answerChoicesEl.textContent = questions[increment].answers[i]
+            //answerChoicesEl.append(choicesEl)
 
-            answerChoicesEl.textContent = questions[increment].answerChoicesEl[i]
-            choicesEl.append(answerChoicesEl)
-
-            answerChoicesEl.addEventListener("click", function (e){
+            choicesEl.addEventListener("click", function (e){
                 e.preventDefault()
                 var answerSelected = this.textContent
                 if (answerSelected === questions[increment].correctAnswer) {
                     increment++
                     choicesEl.innerHTML = ""
-                    renderProgress(true)
-                    renderQuestionChoices()
+                    selectAnswer(true)
+                    nextQuestion()
                 } else {
-                    renderProgress(false)
+                    selectAnswer(false)
                     timerlimit = timerlimit - 10;
                 }
                 
@@ -63,41 +98,16 @@ function nextQuestion() {
     }
 }
 
+setTimeout(function(){
+    timerEl.textContent = "Time is Up"
+}, 90000)
+
 //answer function
 function selectAnswer () {
     //if (answers === correctAnswer)
     
 }
 
-//quesitons/answers variables
-var questions = [
-{
-    question: "What does js stand for?",
-    answers: ["just stay", "just smart", "javascript", "joy script"],
-    correctAnswer: "javascript"
-},
-{
-    question: "Which HTML element is correct for JavaScript?",
-    answers: ["<script>", "<js>", "<javascript>", "<java>"],
-    correctAnswer: "0"
-},
-{
-    question: "Where is the correct place to insert a JavaScript?",
-    answers: ["<head>", "<body>", "between <head> and <body>", "all 3 places"],
-    correctAnswer: "<body>"
-},
-{
-    question: "How do you create function in JavaScript?",
-    answers: ["function myFunction ()", "function = myFunction", "function(myFunction())", "myFunction() = function"],
-    correctAnswer: "function myFunction"
-},
-{
-    question: "How do you add a comment in a JavaScript?",
-    answers: ["<!--This is a comment", "//This is a comment", ".This is a comment", "<This is a comment>"],
-    correctAnswer: "//This is a comment"
-},
-
-]
 
 // handle user answer click
     // if answer was correct, then display "correct message"
